ab5a405d583fa377bfe43fb928691f79e99c11f0
Valērijs Stundiņš <vstund@gmail.com>
2021-09-07 22:07:34 +0300

    Add project files.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kā Tevi sauc?");
+
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Sveiks, {myName}");
+
+            //--------------------------------------------------------------------------------------
+
+            Console.WriteLine("Kāds ir tavs vecums?");
+
+            string myAgeTxt = Console.ReadLine();
+
+            int myAge = int.Parse(myAgeTxt);
+
+            int ageNextYear = myAge++;
+
+            bool isAdult = myAge >= 18;
+
+            Console.WriteLine($"Nākamgad Tev paliks {myAge}, Tu esi pilngadīgs: {isAdult}");
+
+
+            //---------------------------------------------------------------------------------------
+
+
+
+
+
+        }
+    }
+}
